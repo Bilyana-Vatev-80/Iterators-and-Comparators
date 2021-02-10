b9c8f19ccc30260787392bebab0d9709d5e6c036
Bilyana Vatev <bilyanavatev@gmail.com>
2021-02-10 12:15:49 +0000

    homework problem

---
+++ src/PO2_Collection/Main.java
@@ -0,0 +1,41 @@
+package PO2_Collection;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner (System.in);
+        List<String> data = Arrays.stream(scanner.nextLine().split("\\s+")).skip(1)
+                .collect(Collectors.toList());
+
+        ListyIterator listyIterator = new ListyIterator(data);
+        String command = scanner.nextLine();
+
+        while (!"END".equals(command)){
+            switch (command){
+                case "Move":
+                    System.out.println(listyIterator.move());
+                    break;
+                case "Print":
+                    System.out.println(listyIterator.print());
+                    break;
+                case "HasNext":
+                    System.out.println(listyIterator.iterator().hasNext());
+                    break;
+                case "PrintAll":
+                    Iterator <String> iterator = listyIterator.iterator();
+                    while (iterator.hasNext()){
+                        System.out.print(iterator.next() + " ");
+                    }
+                    System.out.println();
+                    break;
+            }
+            command = scanner.nextLine();
+        }
+    }
+}
+
+
